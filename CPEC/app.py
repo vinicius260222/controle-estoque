83955a8b7a002cc694ac2240dc463179d9609293
vinicius260222 <vinicius.gabriel.silva.domingos@gmail.com>
2026-01-13 22:54:05 -0300

    Update app.py

--- CPEC/app.py
+++ CPEC/app.py
@@ -115,6 +115,10 @@ def exportar_excel():
         download_name="estoque.xlsx"
     )
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
